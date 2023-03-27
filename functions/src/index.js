3d3428d715c2a429541434e2006623434717025d
fG40912 <futureg412@gmail.com>
2023-03-27 21:58:21 +1030

    Firestore integration through firebase-admin SDK
    What?
    The error before was poor reading skills on Firebase Firestore Doc

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore')
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = initializeApp(
+    // {
+    //     credential: admin.credential.cert(serviceAccount),
+    //     // The database URL depends on the location of the database
+    //     databaseURL: "https://DATABASE_NAME.firebaseio.com"
+    // }
+)
+const db = getFirestore()
+
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+    let challengesRef = db.collection("challenges");
+    let snapshot = await challengesRef.get();
+
+    let result = [];
+    snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        result.push(doc.data())
+    });
+    response.send(JSON.stringify(result));
+});
